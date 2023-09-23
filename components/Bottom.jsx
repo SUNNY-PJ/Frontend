f3d3d0e0efa92867163d2ebce16742f0b47e61c7
KwonSuyeon <commetoi_yeoni@naver.com>
2023-09-24 03:44:39 +0900

    fix: 하단 버튼 추가

--- components/Bottom.jsx
+++ components/Bottom.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   View,
   Image,
@@ -6,59 +6,126 @@ import {
   ImageBackground,
   TouchableOpacity,
   StyleSheet,
+  Animated,
 } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
 // const Bottom = ({ navigation }) => {
 const Bottom = () => {
   const navigation = useNavigation();
+  const [leftIconVisible, setLeftIconVisible] = useState(false);
+  const [rightIconVisible, setRightIconVisible] = useState(false);
+
+  const toggleIcons = () => {
+    setLeftIconVisible(!leftIconVisible);
+    setRightIconVisible(!rightIconVisible);
+  };
+
+  const rotateRightIcon = () => {
+    Animated.sequence([
+      Animated.timing(rightIconRotation, {
+        toValue: 45,
+        duration: 300,
+        useNativeDriver: false,
+      }),
+      Animated.timing(rightIconRotation, {
+        toValue: 0,
+        duration: 300,
+        useNativeDriver: false,
+      }),
+    ]).start();
+  };
 
   return (
-    <ImageBackground
-      source={require("../assets/bottom.png")}
-      style={styles.container}
-    >
-      <TouchableOpacity
-        style={styles.menuItem}
-        onPress={() => navigation.navigate("Note")}
+    <View>
+      <View
+        style={{
+          flexDirection: "row",
+          gap: 40,
+          bottom: 130,
+          alignSelf: "center",
+        }}
       >
-        <Image
-          source={require("../assets/community.png")}
-          style={styles.icon}
-        />
-        <Text style={styles.menuText}>커뮤니티</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.menuItem}
-        onPress={() => navigation.navigate("Statistics")}
+        {leftIconVisible && (
+          <TouchableOpacity
+            activeOpacity={1}
+            onPress={() => navigation.navigate("Login")}
+          >
+            <Image
+              source={require("../assets/lefrBtn.png")}
+              style={{ width: 64, height: 80 }}
+            />
+          </TouchableOpacity>
+        )}
+        {rightIconVisible && (
+          <TouchableOpacity activeOpacity={1}>
+            <Image
+              source={require("../assets/rightBtn.png")}
+              style={{ width: 64, height: 80 }}
+            />
+          </TouchableOpacity>
+        )}
+      </View>
+      <ImageBackground
+        source={require("../assets/bottom.png")}
+        style={styles.container}
       >
-        <Image source={require("../assets/message.png")} style={styles.icon} />
-        <Text style={styles.menuText}>대화</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.menuItem}
-        onPress={() => navigation.navigate("Login")}
-      >
-        <Image
-          source={require("../assets/bottomIcon.png")}
-          style={{ width: 64, height: 64, top: -50 }}
-        />
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.menuItem}
-        onPress={() => navigation.navigate("FriendsList")}
-      >
-        <Image source={require("../assets/friends.png")} style={styles.icon} />
-        <Text style={styles.menuText}>친구</Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.menuItem}
-        onPress={() => navigation.navigate("FriendsList")} // 마이페이지 스크린으로 이동
-      >
-        <Image source={require("../assets/profile.png")} style={styles.icon} />
-        <Text style={styles.menuText}>마이페이지</Text>
-      </TouchableOpacity>
-    </ImageBackground>
+        <TouchableOpacity
+          activeOpacity={1}
+          style={styles.menuItem}
+          onPress={() => navigation.navigate("Note")}
+        >
+          <Image
+            source={require("../assets/community.png")}
+            style={styles.icon}
+          />
+          <Text style={styles.menuText}>커뮤니티</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          activeOpacity={1}
+          style={styles.menuItem}
+          onPress={() => navigation.navigate("Statistics")}
+        >
+          <Image
+            source={require("../assets/message.png")}
+            style={styles.icon}
+          />
+          <Text style={styles.menuText}>대화</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          activeOpacity={1}
+          style={styles.menuItem}
+          onPress={toggleIcons}
+        >
+          <Image
+            source={require("../assets/bottomIcon.png")}
+            style={{ width: 64, height: 64, top: -50 }}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity
+          activeOpacity={1}
+          style={styles.menuItem}
+          onPress={() => navigation.navigate("FriendsList")}
+        >
+          <Image
+            source={require("../assets/friends.png")}
+            style={styles.icon}
+          />
+          <Text style={styles.menuText}>친구</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          activeOpacity={1}
+          style={styles.menuItem}
+          onPress={() => navigation.navigate("FriendsList")} // 마이페이지 스크린으로 이동
+        >
+          <Image
+            source={require("../assets/profile.png")}
+            style={styles.icon}
+          />
+          <Text style={styles.menuText}>마이페이지</Text>
+        </TouchableOpacity>
+      </ImageBackground>
+    </View>
   );
 };
 
