61b1f004ff576fc2244fcf23c3afe0281c97b785
KwonSuyeon <commetoi_yeoni@naver.com>
2023-09-25 00:04:35 +0900

    feat: 커뮤니티 페이지 생성

---
+++ constant/Community/board.jsx
@@ -0,0 +1,110 @@
+import React, { useState } from "react";
+import {
+  View,
+  Image,
+  Text,
+  ImageBackground,
+  TouchableOpacity,
+  StyleSheet,
+  Animated,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+
+const Board = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.contentContainer}>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            paddingLeft: 16,
+            paddingRight: 24,
+            marginTop: 5,
+            marginBottom: 18,
+          }}
+        >
+          <View style={{ flexDirection: "row", gap: 3 }}>
+            <Image
+              source={require("../../assets/sort.png")}
+              style={{
+                width: 20,
+                height: 20,
+              }}
+            />
+            <Text
+              style={{
+                color: "#262626",
+                fontSize: 15,
+                fontWeight: 500,
+                alignSelf: "center",
+              }}
+            >
+              최신순
+            </Text>
+          </View>
+          <View
+            style={{
+              flexDirection: "row",
+              gap: 22,
+            }}
+          >
+            <Image
+              source={require("../../assets/search.png")}
+              style={{
+                width: 20,
+                height: 20,
+              }}
+            />
+            <Image
+              source={require("../../assets/write.png")}
+              style={{
+                width: 20,
+                height: 20,
+              }}
+            />
+          </View>
+        </View>
+        <View style={styles.tabBar}>
+          <View style={styles.tabBarLine} />
+        </View>
+        <View>
+          <Text style={{ fontSize: 20, color: "#000", fontWeight: 500 }}>
+            test
+          </Text>
+        </View>
+        <View style={styles.tabBar}>
+          <View style={styles.tabBarLine} />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "column",
+    backgroundColor: "#FFFBF6",
+    minHeight: "100%",
+  },
+  contentContainer: {
+    marginBottom: 40,
+  },
+  image: {
+    width: 32,
+    height: 32,
+    alignSelf: "center",
+    marginTop: 12,
+  },
+  tabBar: {
+    flexDirection: "row",
+    marginBottom: 12,
+  },
+  tabBarLine: {
+    flex: 1,
+    height: 2,
+    backgroundColor: "#C1C1C1",
+  },
+});
+
+export default Board;
---
+++ constant/Community/community.jsx
@@ -0,0 +1,94 @@
+import { useState } from "react";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+import Bar from "../../components/Bar";
+import Statistics from "../statistics";
+import Board from "./board";
+
+const Community = () => {
+  const [board, setBoard] = useState(false);
+  const [statistics, setStatistics] = useState(true);
+
+  const historyClick = () => {
+    setBoard(true);
+    setStatistics(false);
+  };
+
+  const statisticsClick = () => {
+    setStatistics(true);
+    setBoard(false);
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.contentContainer}>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "center",
+            gap: 116,
+            marginTop: 25,
+            marginBottom: 18,
+          }}
+        >
+          <TouchableOpacity onPress={statisticsClick} activeOpacity={0.6}>
+            <Text style={[styles.tabText, statistics && styles.activeTabText]}>
+              절약 꿀팁
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={historyClick} activeOpacity={0.6}>
+            <Text style={[styles.tabText, board && styles.activeTabText]}>
+              자유게시판
+            </Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.tabBar}>
+          <View
+            style={[styles.tabBarLine, statistics && styles.activeTabBarLine]}
+          />
+          <View style={[styles.tabBarLine, board && styles.activeTabBarLine]} />
+        </View>
+        {statistics && <Statistics />}
+        {board && <Board />}
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "column",
+    backgroundColor: "#FFFBF6",
+    minHeight: "100%",
+  },
+  contentContainer: {
+    marginBottom: 40,
+  },
+  tabText: {
+    fontSize: 20,
+    fontWeight: "700",
+    color: "#C1C1C1",
+  },
+  activeTabText: {
+    color: "#1F1F1F",
+  },
+  tabBar: {
+    flexDirection: "row",
+    marginBottom: 12,
+  },
+  tabBarLine: {
+    flex: 1,
+    height: 2,
+    backgroundColor: "#C1C1C1",
+  },
+  activeTabBarLine: {
+    backgroundColor: "#1F1F1F",
+  },
+  image: {
+    width: 32,
+    height: 32,
+    alignSelf: "center",
+    marginTop: 12,
+  },
+});
+
+export default Community;
