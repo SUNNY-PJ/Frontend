2aa4e99b9b857caaff03d86f9f8871539c9b1f33
KwonSuyeon <commetoi_yeoni@naver.com>
2023-09-26 00:29:41 +0900

    feat: sdk 카카오 로그인 구현

--- constant/kakao.jsx
+++ constant/kakao.jsx
@@ -5,35 +5,54 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useNavigation } from "@react-navigation/native";
 import axios from "axios";
 
-// const REST_API_KEY = "56e15a4c7aaa857397437034b58c0016";
-// const REDIRECT_URI = "http://192.168.50.45:19006";
-// const INJECTED_JAVASCRIPT = `window.ReactNativeWebView.postMessage('message from webView')`;
+const REST_API_KEY = "56e15a4c7aaa857397437034b58c0016";
+const REDIRECT_URI = "http://192.168.50.45:19006";
+const INJECTED_JAVASCRIPT = `window.ReactNativeWebView.postMessage('message from webView')`;
 
 const Kakao = () => {
   console.log("페이지 진입");
   const navigation = useNavigation();
 
-  const fetchData = async () => {
-    try {
-      const response = await axios.get(
-        "http://43.201.176.22:8080/oauth2/authorize/kakao?redirect_uri=http://43.201.176.22:8080/auth/token"
-      );
-      console.log("데이터:", response);
-      console.log("데이터1111:", response.data.data);
-
-      // console.log("데이터:", response.data);
-    } catch (error) {
-      console.error("에러:", error);
+  // 로그인 페이지(임시로 지출현황 버튼 클릭 후 로그인 페이지 진입)에서
+  // 카카오 로그인 버튼 누른 후 로그인까지 해야 코드/토큰 발급 받을 수 있음
+  function KakaoLoginWebView(data) {
+    console.log("카카오 로그인 시도:::");
+    const exp = "code=";
+    var condition = data.indexOf(exp);
+    if (condition != -1) {
+      var authorize_code = data.substring(condition + exp.length);
+      console.log("코드:::", authorize_code);
+      requestToken(authorize_code);
     }
-  };
+  }
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+  const requestToken = async (authorize_code) => {
+    var AccessToken = "none";
+    axios({
+      method: "post",
+      url: "https://kauth.kakao.com/oauth/token",
+      params: {
+        grant_type: "authorization_code",
+        client_id: REST_API_KEY,
+        redirect_uri: REDIRECT_URI,
+        code: authorize_code,
+      },
+    })
+      .then((response) => {
+        AccessToken = response.data.access_token;
+        console.log("토큰:::", AccessToken);
+        requestUserInfo(AccessToken);
+      })
+      .catch(function (error) {
+        console.log("error", error);
+      });
+    // 로그인 성공 후 코드/토큰 발급 받으면 친구 목록으로 이동함
+    navigation.navigate("FriendsList", { screen: "FriendsList" });
+  };
 
   return (
     <View style={Styles.container}>
-      {/* <WebView
+      <WebView
         style={{ flex: 1 }}
         originWhitelist={["*"]}
         scalesPageToFit={false}
@@ -45,7 +64,7 @@ const Kakao = () => {
         onMessage={(event) => {
           KakaoLoginWebView(event.nativeEvent["url"]);
         }}
-      /> */}
+      />
     </View>
   );
 };
