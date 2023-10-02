0c450aa8315d0123e2b04dfe0b0b67505f12b238
KwonSuyeon <commetoi_yeoni@naver.com>
2023-10-03 02:09:38 +0900

    feat: 승리/패배 메세지 모달 생성

---
+++ components/Modal/loserModal.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  Modal,
+  TouchableOpacity,
+} from "react-native";
+
+const LoserModal = ({ isOpenLoserModal, openLoserModal }) => {
+  return (
+    <Modal animationType="slide" transparent={true} visible={isOpenLoserModal}>
+      <View
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+          // backgroundColor: "rgba(0, 0, 0, 0.5)",
+          height: "100%",
+        }}
+      >
+        <View style={styles.container}>
+          <View style={styles.closeButtonContainer}>
+            <TouchableOpacity onPress={openLoserModal}>
+              <Image
+                source={require("../../assets/close.png")}
+                style={{ width: 16, height: 16 }}
+              />
+            </TouchableOpacity>
+          </View>
+          <View style={styles.content}>
+            <Image
+              source={require("../../assets/loser.png")}
+              style={{ width: 110, height: 102, alignSelf: "center" }}
+            />
+            <Text style={styles.title}>
+              유림님과의 대결에서
+              <Text style={{ color: "#D32F2F" }}>졌어요</Text>
+            </Text>
+          </View>
+        </View>
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: 335,
+    borderRadius: 16,
+    borderWidth: 1.5,
+    borderColor: "#1F1F1F",
+    backgroundColor: "#fff",
+    borderBottomWidth: 3,
+    borderRightWidth: 3,
+  },
+  closeButtonContainer: {
+    alignSelf: "flex-end",
+    paddingTop: 16,
+    paddingRight: 16,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "700",
+    marginBottom: 24,
+    textAlign: "center",
+    color: "#000",
+  },
+  content: {
+    gap: 21,
+  },
+});
+
+export default LoserModal;
---
+++ components/Modal/winnerModal.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  Modal,
+  TouchableOpacity,
+} from "react-native";
+
+const WinnerModal = ({ isOpenWinnerModal, openWinnerModal }) => {
+  return (
+    <Modal animationType="slide" transparent={true} visible={isOpenWinnerModal}>
+      <View
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+          // backgroundColor: "rgba(0, 0, 0, 0.5)",
+          height: "100%",
+        }}
+      >
+        <View style={styles.container}>
+          <View style={styles.closeButtonContainer}>
+            <TouchableOpacity onPress={openWinnerModal}>
+              <Image
+                source={require("../../assets/close.png")}
+                style={{ width: 16, height: 16 }}
+              />
+            </TouchableOpacity>
+          </View>
+          <View style={styles.content}>
+            <Image
+              source={require("../../assets/winner.png")}
+              style={{ width: 109, height: 102, alignSelf: "center" }}
+            />
+            <Text style={styles.title}>
+              민지님과의 대결에서
+              <Text style={{ color: "#007560" }}>이겼어요 </Text>
+            </Text>
+          </View>
+        </View>
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: 335,
+    borderRadius: 16,
+    borderWidth: 1.5,
+    borderColor: "#1F1F1F",
+    backgroundColor: "#fff",
+    borderBottomWidth: 3,
+    borderRightWidth: 3,
+  },
+  closeButtonContainer: {
+    alignSelf: "flex-end",
+    paddingTop: 16,
+    paddingRight: 16,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "700",
+    marginBottom: 24,
+    textAlign: "center",
+    color: "#000",
+  },
+  content: {
+    gap: 21,
+  },
+});
+
+export default WinnerModal;
