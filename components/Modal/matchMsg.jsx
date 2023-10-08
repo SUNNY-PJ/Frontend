5c4c0a818199001db0e3f889aaa920d20a0d9862
KwonSuyeon <commetoi_yeoni@naver.com>
2023-10-09 04:03:20 +0900

    fix: 키보드 영역만큼 올라가도록 수정

--- components/Modal/matchMsg.jsx
+++ components/Modal/matchMsg.jsx
@@ -1,10 +1,64 @@
-import React, { useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  KeyboardAvoidingView,
+  Platform,
+  Keyboard,
+} from "react-native";
 import Input from "../Input/input";
 import LargeBtnDisable from "../Btn/largeBtnDisable";
+import LargeBtn from "../Btn/largeBtn";
 import Modal from "react-native-modal";
 
 const MatchMsg = ({ isMatchModal, matchModal }) => {
+  const [date, setDate] = useState("");
+  const [message, setMessage] = useState("");
+  const [money, setMoney] = useState("");
+  const [compensation, setCompensation] = useState("");
+  const [isAllFieldsFilled, setIsAllFieldsFilled] = useState(false);
+
+  const handleDateChange = (text) => {
+    setDate(text);
+  };
+
+  const handleMessageChange = (text) => {
+    setMessage(text);
+  };
+
+  const handleCompensationChange = (text) => {
+    setCompensation(text);
+  };
+
+  const formattedMoney = (value) => {
+    const parsedValue = parseFloat(value.replace(/,/g, ""));
+    if (!isNaN(parsedValue)) {
+      return parsedValue.toLocaleString();
+    } else {
+      return "";
+    }
+  };
+
+  // money 상태 변수 업데이트
+  const handleMoneyChange = (text) => {
+    const cleanedText = text.replace(/[^0-9,]/g, "");
+    const formattedValue = formattedMoney(cleanedText);
+    setMoney(formattedValue);
+  };
+
+  const handlePostApiTestStart = () => {
+    alert("등록");
+  };
+
+  useEffect(() => {
+    if (message && money && date) {
+      setIsAllFieldsFilled(true);
+    } else {
+      setIsAllFieldsFilled(false);
+    }
+  }, [message, money, date, compensation]);
+
   return (
     <Modal
       isVisible={isMatchModal}
@@ -12,10 +66,12 @@ const MatchMsg = ({ isMatchModal, matchModal }) => {
       animationOut="slideOutDown"
       onSwipeComplete={matchModal}
       swipeDirection="down"
-      //   backdropColor="transparent"
-      style={styles.modal} // 모달 스타일 추가
+      style={styles.modal}
     >
-      <View style={styles.modalContainer}>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.modalContainer}
+      >
         <View
           style={{
             width: 64,
@@ -39,25 +95,45 @@ const MatchMsg = ({ isMatchModal, matchModal }) => {
         </Text>
         <View style={styles.modalContent}>
           <Text style={styles.modalText}>친구를 도발해보세요!</Text>
-          <Input placeholder={"도발 메세지"} />
+          <Input
+            placeholder={"도발 메세지"}
+            inputValue={message}
+            handleInputChange={handleMessageChange}
+          />
           <Text style={styles.subText}>* 최대 20자</Text>
         </View>
         <View style={styles.modalContent}>
           <Text style={styles.modalText}>무엇을 걸고 대결할까요?</Text>
-          <Input placeholder={"대결 보상"} />
+          <Input
+            placeholder={"대결 보상"}
+            inputValue={compensation}
+            handleInputChange={handleCompensationChange}
+          />
         </View>
         <View style={styles.modalContent}>
           <Text style={styles.modalText}>대결 기간과 금액을 설정해 주세요</Text>
-          <Input placeholder={"대결 기간"} />
+          <Input
+            placeholder={"대결 기간"}
+            inputValue={date}
+            handleInputChange={handleDateChange}
+          />
           <Text style={styles.subText}>
             * 상대가 승낙한 시점부터 대결이 시작됩니다
           </Text>
-          <Input placeholder={"대결 금액"} />
+          <Input
+            placeholder={"대결 금액"}
+            inputValue={money}
+            handleInputChange={handleMoneyChange}
+          />
         </View>
         <View style={{ marginBottom: 31 }}>
-          <LargeBtnDisable text={"전송하기"} />
+          {isAllFieldsFilled ? (
+            <LargeBtn text={"등록하기"} onClick={handlePostApiTestStart} />
+          ) : (
+            <LargeBtnDisable text={"전송하기"} />
+          )}
         </View>
-      </View>
+      </KeyboardAvoidingView>
     </Modal>
   );
 };
