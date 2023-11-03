c48c2bff7be01cbfc004ae386419bbea2bad213c
KwonSuyeon <commetoi_yeoni@naver.com>
2023-11-04 03:58:57 +0900

    feat: 커뮤니티 api 추가

--- screen/Community/community.jsx
+++ screen/Community/community.jsx
@@ -1,11 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
-import Bar from "../../components/Bar";
-import Statistics from "../../constant/statistics";
+import { proxyUrl } from "../../constant/common";
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import Board from "./board";
 import Tip from "./tip";
 
 const Community = () => {
+  const inputURL = "/community";
+  const cleanedURL = inputURL.replace(/[\u200B]/g, "");
+
+  const url = proxyUrl + inputURL;
+
   const [board, setBoard] = useState(false);
   const [tip, setTip] = useState(true);
 
@@ -18,6 +24,37 @@ const Community = () => {
     setTip(true);
     setBoard(false);
   };
+
+  const fetchData = async () => {
+    const access_token = await AsyncStorage.getItem("access_token");
+    console.log(access_token);
+    console.log("get 실행");
+
+    try {
+      const response = await axios.get(url, {
+        headers: {
+          "Content-Type": "application/json; charset=utf-8",
+          Authorization: `Bearer ${access_token}`,
+        },
+        // params: {
+        //   page: 1,
+        //   size: 10,
+        //   sort: "",
+        //   sortType: "최신순",
+        //   boardType: "꿀팁"
+        // },
+      });
+
+      console.log("데이터:", response.data);
+    } catch (error) {
+      console.error("에러:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+    console.log("실행된건가");
+  }, []);
 
   return (
     <View style={styles.container}>
