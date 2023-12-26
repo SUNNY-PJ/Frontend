65bce1017a6119ae0b5932b4d9a8520f4cefd1d6
KwonSuyeon <commetoi_yeoni@naver.com>
2023-12-27 00:40:48 +0900

    fix: 댓글 data 조회

--- screen/MyPage/myComment.jsx
+++ screen/MyPage/myComment.jsx
@@ -42,46 +42,18 @@ const MyComment = () => {
 
   return (
     <View style={styles.container}>
-      <View style={styles.box}>
-        <Text style={styles.title}>댓글 내용 댓글 내내용용</Text>
-        <View style={{ flexDirection: "row" }}>
-          <Text style={styles.description}>짱구</Text>
-          <Text style={styles.description}>2023.11.17 17:19</Text>
+      {data.map((item) => (
+        <View>
+          <View style={styles.box}>
+            <Text style={styles.title}>{item.content}</Text>
+            <View style={{ flexDirection: "row" }}>
+              <Text style={styles.description}>{item.writer}</Text>
+              <Text style={styles.description}>2023.11.17 17:19</Text>
+            </View>
+          </View>
+          <Line color={"#C1C1C1"} h={1} />
         </View>
-      </View>
-      <Line color={"#C1C1C1"} h={1} />
-      <View style={styles.box}>
-        <Text style={styles.title}>여긴 댓글 내용임</Text>
-        <View style={{ flexDirection: "row" }}>
-          <Text style={styles.description}>뿡뿡이는 무얼 먹을까</Text>
-          <Text style={styles.description}>2023.11.17 17:19</Text>
-        </View>
-      </View>
-      <Line color={"#C1C1C1"} h={1} />
-      <View style={styles.box}>
-        <Text style={styles.title}>댓글임</Text>
-        <View style={{ flexDirection: "row" }}>
-          <Text style={styles.description}>롯데월드 갈 사람</Text>
-          <Text style={styles.description}>2023.09.09 09:23</Text>
-        </View>
-      </View>
-      <Line color={"#C1C1C1"} h={1} />
-      <View style={styles.box}>
-        <Text style={styles.title}>내가 쓴 댓글임</Text>
-        <View style={{ flexDirection: "row" }}>
-          <Text style={styles.description}>하윙</Text>
-          <Text style={styles.description}>2023.09.09 07:09</Text>
-        </View>
-      </View>
-      <Line color={"#C1C1C1"} h={1} />
-      <View style={styles.box}>
-        <Text style={styles.title}>댓글임</Text>
-        <View style={{ flexDirection: "row" }}>
-          <Text style={styles.description}>알라리알라숑</Text>
-          <Text style={styles.description}>2023.09.09 07:09</Text>
-        </View>
-      </View>
-      <Line color={"#C1C1C1"} h={1} />
+      ))}
     </View>
   );
 };
