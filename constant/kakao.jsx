d05c48c79a9781d2fcd11e9503023a7d3a83d809
KwonSuyeon <commetoi_yeoni@naver.com>
2024-01-04 00:31:37 +0900

    fix: kakao 로직 수정

--- constant/kakao.jsx
+++ constant/kakao.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { View, StyleSheet } from "react-native";
 import { WebView } from "react-native-webview";
 import AsyncStorage from "@react-native-async-storage/async-storage";
@@ -8,8 +8,6 @@ import axios from "axios";
 
 const REST_API_KEY = "56e15a4c7aaa857397437034b58c0016";
 const REDIRECT_URI = "http://192.168.50.45:19006";
-// const REST_API_KEY = "7ff971db2010c97a3e191dd319ec45cd";
-// const REDIRECT_URI = "http://43.201.176.22:8080/auth/kakao/callback";
 const INJECTED_JAVASCRIPT = `window.ReactNativeWebView.postMessage('message from webView')`;
 
 const Kakao = () => {
@@ -20,39 +18,41 @@ const Kakao = () => {
   const kakao_url =
     "https://kauth.kakao.com/oauth/authorize?client_id=7ff971db2010c97a3e191dd319ec45cd&redirect_uri=http://43.201.176.22:8080/auth/kakao/callback&response_type=code";
 
-  // const kakao_url = `https://kauth.kakao.com/oauth/authorize?client_id=7ff971db2010c97a3e191dd319ec45cd&redirect_uri=http://43.201.176.22:8080/auth/kakao/callback&response_type=${code}`;
+  // fetchData, postData를 순서대로 호출하는 함수
+  const handleWebViewMessage = (data) => {
+    KakaoLoginWebView(data);
+    fetchData();
+    postData();
+  };
 
   const fetchData = async () => {
-    console.log("get 실행");
     try {
       const response = await axios.get(kakao_url, {
         headers: {
           "Content-Type": "application/json; charset=utf-8",
         },
       });
-      // if (response) {
-      // if (response.data.data) {
+
       const access_token = response.data.data.accessToken;
       console.log(access_token);
       const refresh_token = response.data.data.refreshToken;
       console.log("accessToken:::::", access_token);
       await AsyncStorage.setItem("access_token", access_token);
       await AsyncStorage.setItem("refresh_token", refresh_token);
-      // }
+
       navigation.navigate("MainScreen", { screen: "SignUp" });
       console.log("데이터:::::", response.data);
-      // }
     } catch (error) {
       console.log("errorMessage:::", error.message);
       if (error.response) {
-        console.error("서버  응답 오류:", error.response);
+        console.error("서버 응답 오류:", error.response);
       } else {
         console.error("에러:", error);
       }
     }
   };
 
-  // 토큰 전송 api
+  // 디바이스 토큰 api
   const postData = async () => {
     const access_token = await AsyncStorage.getItem("access_token");
     const device_token = await AsyncStorage.getItem("device_token");
@@ -68,20 +68,13 @@ const Kakao = () => {
         },
       });
 
-      console.log(response.data);
+      console.log("디바이스 토큰 api", response.data);
 
       navigation.navigate("MainScreen", { screen: "FriendsList" });
     } catch (error) {
       console.error("에러:", error);
     }
   };
-
-  useEffect(() => {
-    fetchData();
-    // if (token) {
-    postData();
-    // }
-  }, []);
 
   function KakaoLoginWebView(data) {
     console.log("카카오 로그인 시도:::");
@@ -94,19 +87,6 @@ const Kakao = () => {
     }
   }
 
-  // 토큰 저장
-  // const storeData = async (returnValue) => {
-  //   try {
-  //     await AsyncStorage.setItem("access_token", returnValue);
-  //     await AsyncStorage.setItem("refresh_token", returnValue);
-  //     const access_token = await AsyncStorage.getItem("access_token");
-  //     console.log("토큰저장:::", access_token);
-  //     console.log("Token stored successfully:", returnValue);
-  //   } catch (error) {
-  //     console.error("Error storing token:", error);
-  //   }
-  // };
-
   return (
     <View style={Styles.container}>
       <WebView
@@ -115,12 +95,11 @@ const Kakao = () => {
         scalesPageToFit={false}
         source={{
           uri: kakao_url,
-          // uri: `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}`,
         }}
         injectedJavaScript={INJECTED_JAVASCRIPT}
         javaScriptEnabled
         onMessage={(event) => {
-          KakaoLoginWebView(event.nativeEvent["url"]);
+          handleWebViewMessage(event.nativeEvent["url"]);
         }}
       />
     </View>
