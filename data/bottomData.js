4f48b7a029b1c80813e43d3ef40fb06af7190d0f
KwonSuyeon <commetoi_yeoni@naver.com>
2024-01-13 23:53:05 +0900

    fix: 파일명 변경

--- data/bottomData.js
+++
@@ -1,4 +0,0 @@
-export const COMMUNITY_CATEGORY = [
-  { category: "절약 꿀팁" },
-  { category: "자유 게시판" },
-];
---
+++ data/communityData.js
@@ -0,0 +1,4 @@
+export const COMMUNITY_CATEGORY = [
+  { title: "절약 꿀팁" },
+  { title: "자유 게시판" },
+];
