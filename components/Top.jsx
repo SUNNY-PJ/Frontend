a3a2b24a2e5cea8084210055ccd54869c8b5d9b1
KwonSuyeon <commetoi_yeoni@naver.com>
2024-01-19 00:53:04 +0900

    feat: 상단 progressBar 애니메이션 적용

--- components/Top.jsx
+++ components/Top.jsx
@@ -1,9 +1,32 @@
-import React, { useState } from "react";
-import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
+import React, { useState, useRef, useEffect } from "react";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  Animated,
+} from "react-native";
 import * as Notifications from "expo-notifications";
 
 const Top = ({ navigation }) => {
-  const [progress, setProgress] = useState(50);
+  const [progress, setProgress] = useState(30);
+  const progressAnim = useRef(new Animated.Value(0)).current; // 초기 값 0으로 설정
+
+  useEffect(() => {
+    // progress 상태가 변경될 때마다 애니메이션을 실행
+    Animated.timing(progressAnim, {
+      toValue: progress, // 최종 값은 progress 상태 값으로 설정
+      duration: 500, // 애니메이션 지속 시간
+      useNativeDriver: false, // width 속성을 애니메이션하기 때문에 false로 설정
+    }).start();
+  }, [progress]); // progress 값이 바뀔 때마다 애니메이션을 실행
+
+  // progressAnim 값을 width 퍼센테이지로 변환
+  const progressWidth = progressAnim.interpolate({
+    inputRange: [0, 100],
+    outputRange: ["0%", "100%"],
+  });
 
   return (
     <View style={styles.container}>
@@ -11,11 +34,11 @@ const Top = ({ navigation }) => {
         <Text style={styles.text}>D - {progress}%</Text>
         <TouchableOpacity onPress={() => navigation.navigate("Goal")}>
           <View style={styles.progressBar}>
-            <View
+            <Animated.View
               style={[
                 styles.progressFill,
                 {
-                  width: `${progress}%`,
+                  width: progressWidth,
                 },
               ]}
             />
@@ -28,7 +51,7 @@ const Top = ({ navigation }) => {
                 transform: [{ translateY: 0 }, { translateX: 90 }],
               }}
             >
-              NN%
+              {progress}%
             </Text>
           </View>
         </TouchableOpacity>
