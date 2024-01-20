6a13d0c17e965912a594750ab80e09341988e8a5
KwonSuyeon <commetoi_yeoni@naver.com>
2024-01-21 00:51:20 +0900

    feat: animation 추가

--- components/Bar.jsx
+++ components/Bar.jsx
@@ -1,18 +1,36 @@
-import React, { useState } from "react";
-import { View, Text, Image, StyleSheet } from "react-native";
+import React, { useState, useRef, useEffect } from "react";
+import { View, Text, Image, StyleSheet, Animated } from "react-native";
 
 const Bar = ({ text, path, progress }) => {
   // const [progress, setProgress] = useState(50);
+  const progressAnim = useRef(new Animated.Value(0)).current;
   console.log("퍼센트", progress);
-  const progressWidth = (progress / 100) * 335;
+
+  useEffect(() => {
+    Animated.timing(progressAnim, {
+      toValue: progress,
+      duration: 500,
+      useNativeDriver: false,
+    }).start();
+  }, [progress]);
+
+  const progressWidth = progressAnim.interpolate({
+    inputRange: [0, 100],
+    outputRange: ["0%", "100%"],
+  });
+
+  // 숫자에 세 자리마다 쉼표를 추가하는 함수
+  const formatNumberWithCommas = (number) => {
+    return new Intl.NumberFormat().format(number);
+  };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>{text}원</Text>
+      <Text style={styles.text}>{formatNumberWithCommas(text)}원</Text>
       <View style={styles.progressContainer}>
         {/* <Image source={{ uri: path }} style={styles.image} />*/}
         <View style={styles.progressBar}>
-          <View
+          <Animated.View
             style={[
               styles.progressFill,
               {
