c48e457149dbae8f3e6996d0653fd543a77bec7e
KwonSuyeon <commetoi_yeoni@naver.com>
2024-01-21 23:06:31 +0900

    feat: scroll 추가

--- screen/Statistic/history.jsx
+++ screen/Statistic/history.jsx
@@ -1,35 +1,58 @@
 import { useState } from "react";
-import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
 import CalendarComponent from "../../components/Calendar/calendar";
+import { useEffect } from "react";
 
 const History = () => {
   const [data, setData] = useState([]);
+  // 숫자에 세 자리마다 쉼표를 추가하는 함수
+  const formatNumberWithCommas = (number) => {
+    return new Intl.NumberFormat().format(number);
+  };
 
   const onDataFetched = (data) => {
     console.log("받아온 데이터::", data);
     setData(data);
   };
+
+  useEffect(() => {
+    onDataFetched;
+  }, [data]);
+
   return (
     <View style={styles.container}>
       <View style={styles.contentContainer}>
         <CalendarComponent onDataFetched={onDataFetched} />
         {/* <RangeCalendar /> */}
       </View>
-      {data.map((item, index) => (
-        <View style={styles.bottomSection} key={item.index}>
-          <View style={styles.bottomBar} />
-          <View
-            style={{
-              flexDirection: "row",
-              justifyContent: "space-between",
-              width: "100%",
-            }}
-          >
-            <Text style={styles.bottomText}>{item.name}</Text>
-            <Text style={styles.bottomPriceText}>{item.money}원</Text>
-          </View>
-        </View>
-      ))}
+      {/* {data.map((item, index) => ( */}
+      <ScrollView>
+        {Array.isArray(data) &&
+          data.map((item, index) => (
+            <View style={styles.bottomSection} key={index}>
+              <View style={styles.bottomBar} />
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                  width: "100%",
+                }}
+              >
+                <Text style={styles.bottomText}>{item.name}</Text>
+                <Text style={styles.bottomPriceText}>
+                  {formatNumberWithCommas(item.money)}원
+                </Text>
+              </View>
+            </View>
+          ))}
+      </ScrollView>
     </View>
   );
 };
