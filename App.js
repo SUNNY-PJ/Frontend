b8170fdfcf4d956c31d0ccb41d02df2b9f12e790
KwonSuyeon <commetoi_yeoni@naver.com>
2024-01-29 23:37:34 +0900

    fix: 알림 동의로 변경

--- App.js
+++ App.js
@@ -45,100 +45,100 @@ export default function App() {
   //   return <Splash />;
   // }
 
-  const [isSplashScreen, setIsSplashScreen] = useState(true);
+  // const [isSplashScreen, setIsSplashScreen] = useState(true);
 
-  const handleAnimationEnd = () => {
-    setIsSplashScreen(false);
-  };
-
-  if (isSplashScreen) {
-    return <Splash onAnimationEnd={handleAnimationEnd} />;
-  }
-
-  // 메인 화면 렌더링
-  return <Navigation />;
-
-  // useEffect(() => {
-  //   const registerForPushNotificationsAsync = async () => {
-  //     const { status: existingStatus } =
-  //       await Notifications.getPermissionsAsync();
-  //     let finalStatus = existingStatus;
-
-  //     if (existingStatus !== "granted") {
-  //       const { status } = await Notifications.requestPermissionsAsync();
-  //       finalStatus = status;
-  //     }
-
-  //     if (finalStatus !== "granted") {
-  //       alert("알림을 거부하였습니다. 앱에 대한 알림을 받을 수 없습니다.");
-  //       return;
-  //     }
-
-  //     const { data } = await Notifications.getExpoPushTokenAsync();
-  //     console.log("Expo Push Token:", data);
-  //     await AsyncStorage.setItem("device_token", data);
-  //     const device_token = await AsyncStorage.getItem("device_token");
-  //     console.log("이게 디바이스 토큰이지이이이", device_token);
-
-  //     const expoPushToken = `${data}`;
-
-  //     // 정규 표현식을 사용하여 토큰 값 추출
-  //     const tokenRegex = /\[([^\]]+)\]/;
-  //     const match = expoPushToken.match(tokenRegex);
-
-  //     // match 배열의 두 번째 요소에 토큰 값이 있음
-  //     // 이게 진짜 토큰임
-  //     const token = match && match[1];
-
-  //     console.log("푸시 토큰 값:", token);
-
-  //     return data;
-  //   };
-
-  //   const initPushNotifications = async () => {
-  //     if (Platform.OS === "android") {
-  //       await Notifications.setNotificationChannelAsync("default", {
-  //         name: "default",
-  //         importance: Notifications.AndroidImportance.MAX,
-  //         vibrationPattern: [0, 250, 250, 250],
-  //         lightColor: "#FF231F7C",
-  //       });
-  //     }
-
-  //     const pushToken = await registerForPushNotificationsAsync();
-  //     setPushToken(pushToken);
-  //     console.log(pushToken);
-
-  //     Notifications.addNotificationReceivedListener((notification) => {
-  //       console.log("NOTIFICATION:", notification);
-  //     });
-
-  //     notificationListener.current =
-  //       Notifications.addNotificationReceivedListener((notification) => {
-  //         setNotification(notification);
-  //       });
-
-  //     responseListener.current =
-  //       Notifications.addNotificationResponseReceivedListener((response) => {
-  //         console.log(response);
-  //       });
-  //   };
-
-  //   initPushNotifications();
-
-  //   return () => {
-  //     Notifications.removeNotificationSubscription(
-  //       notificationListener.current
-  //     );
-  //     Notifications.removeNotificationSubscription(responseListener.current);
-  //   };
-  // }, []);
-
-  // 알림 예약 함수 호출
-  // const handleScheduleNotification = async () => {
-  //   const notificationData = "알림 내용을 여기에 입력하세요";
-  //   await schedulePushNotification(notificationData);
+  // const handleAnimationEnd = () => {
+  //   setIsSplashScreen(false);
   // };
 
-  // return <Navigation handleScheduleNotification={handleScheduleNotification} />;
+  // if (isSplashScreen) {
+  //   return <Splash onAnimationEnd={handleAnimationEnd} />;
+  // }
+
+  // // 메인 화면 렌더링
+  // return <Navigation />;
+
+  useEffect(() => {
+    const registerForPushNotificationsAsync = async () => {
+      const { status: existingStatus } =
+        await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+
+      if (existingStatus !== "granted") {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+
+      if (finalStatus !== "granted") {
+        alert("알림을 거부하였습니다. 앱에 대한 알림을 받을 수 없습니다.");
+        return;
+      }
+
+      const { data } = await Notifications.getExpoPushTokenAsync();
+      console.log("Expo Push Token:", data);
+      await AsyncStorage.setItem("device_token", data);
+      const device_token = await AsyncStorage.getItem("device_token");
+      console.log("이게 디바이스 토큰이지이이이", device_token);
+
+      const expoPushToken = `${data}`;
+
+      // 정규 표현식을 사용하여 토큰 값 추출
+      const tokenRegex = /\[([^\]]+)\]/;
+      const match = expoPushToken.match(tokenRegex);
+
+      // match 배열의 두 번째 요소에 토큰 값이 있음
+      // 이게 진짜 토큰임
+      const token = match && match[1];
+
+      console.log("푸시 토큰 값:", token);
+
+      return data;
+    };
+
+    const initPushNotifications = async () => {
+      if (Platform.OS === "android") {
+        await Notifications.setNotificationChannelAsync("default", {
+          name: "default",
+          importance: Notifications.AndroidImportance.MAX,
+          vibrationPattern: [0, 250, 250, 250],
+          lightColor: "#FF231F7C",
+        });
+      }
+
+      const pushToken = await registerForPushNotificationsAsync();
+      setPushToken(pushToken);
+      console.log(pushToken);
+
+      Notifications.addNotificationReceivedListener((notification) => {
+        console.log("NOTIFICATION:", notification);
+      });
+
+      notificationListener.current =
+        Notifications.addNotificationReceivedListener((notification) => {
+          setNotification(notification);
+        });
+
+      responseListener.current =
+        Notifications.addNotificationResponseReceivedListener((response) => {
+          console.log(response);
+        });
+    };
+
+    initPushNotifications();
+
+    return () => {
+      Notifications.removeNotificationSubscription(
+        notificationListener.current
+      );
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+
+  // 알림 예약 함수 호출
+  const handleScheduleNotification = async () => {
+    const notificationData = "알림 내용을 여기에 입력하세요";
+    await schedulePushNotification(notificationData);
+  };
+
+  return <Navigation handleScheduleNotification={handleScheduleNotification} />;
 }
