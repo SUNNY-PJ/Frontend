7f4e6f029b66a9faed5104bbf44c5dba51285def
KwonSuyeon <commetoi_yeoni@naver.com>
2024-02-10 03:02:04 +0900

    fix: 신고 결과 메세지 ui 수정

--- components/Modal/report/reportResult.jsx
+++ components/Modal/report/reportResult.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { View, Text, StyleSheet, Modal, TouchableOpacity } from "react-native";
+import RegularBtnOrange from "../../Btn/regularBtnOrange";
 
-const ReportMsg = ({ isVisible, onCancel, result }) => {
+const ReportResult = ({ isVisible, onCancel, result }) => {
   return (
     <Modal animationType="slide" transparent={true} visible={isVisible}>
       <View
@@ -19,59 +20,58 @@ const ReportMsg = ({ isVisible, onCancel, result }) => {
               paddingRight: 20,
             }}
           >
-            <Text style={styles.title}>신고 결과</Text>
+            <Text style={[styles.title, { marginTop: 30, marginBottom: 30 }]}>
+              신고 결과
+            </Text>
             {result === "good" ? (
-              <Text style={styles.text}>
+              <Text style={[styles.text]}>
                 운영 원칙에 어긋나지 않는다고{"\n"}
                 판단되어 경고를 보내지 않았습니다.{"\n"}
                 깨끗한 커뮤니티 이용을 위해{"\n"}
                 힘써주셔서 감사합니다!
               </Text>
             ) : (
-              <Text style={styles.text}>
+              <Text style={[styles.text]}>
                 작성자에게 경고를 보냈습니다.{"\n"}
                 깨끗한 커뮤니티 이용을 위해{"\n"}
                 힘써주셔서 감사합니다!
               </Text>
             )}
             <View
-              style={{ height: 1, backgroundColor: "#C1C1C1", width: 80 }}
+              style={{
+                height: 1,
+                backgroundColor: "#C1C1C1",
+                width: 80,
+                alignSelf: "center",
+                marginTop: 20,
+                marginBottom: 20,
+              }}
             />
-            <View style={{ flexDirection: "column", gap: 8 }}>
+            <View style={{ flexDirection: "column", gap: 15 }}>
               <View style={{ flexDirection: "column", gap: 5 }}>
-                <Text style={styles.contentTitle}>신고 일자</Text>
-                <Text style={styles.contentTitle}>0000-00-00</Text>
+                <Text style={[styles.contentTitle]}>신고 일자</Text>
+                <Text style={[styles.contentText]}>0000-00-00</Text>
               </View>
               <View style={{ flexDirection: "column", gap: 5 }}>
-                <Text style={styles.contentTitle}>
+                <Text style={[styles.contentTitle]}>
                   신고 대상 (작성자 닉네임)
                 </Text>
-                <Text style={styles.contentTitle}>닉네임</Text>
+                <Text style={[styles.contentText]}>닉네임</Text>
               </View>
               <View style={{ flexDirection: "column", gap: 5 }}>
-                <Text style={styles.contentTitle}>신고 내용</Text>
-                <Text style={styles.contentTitle}>게시글/댓글/답글 내용</Text>
+                <Text style={[styles.contentTitle]}>신고 내용</Text>
+                <Text style={[styles.contentText]}>게시글/댓글/답글 내용</Text>
               </View>
               <View style={{ flexDirection: "column", gap: 5 }}>
-                <Text style={styles.contentTitle}>신고 사유</Text>
-                <Text style={styles.contentTitle}>어쩌구저쩌구...</Text>
+                <Text style={[styles.contentTitle]}>신고 사유</Text>
+                <Text style={[styles.contentText]}>어쩌구저쩌구...</Text>
               </View>
             </View>
-            <TouchableOpacity onPress={onCancel}>
-              <View
-                style={{
-                  width: 160,
-                  height: 40,
-                  borderColor: "#FFC891",
-                  borderWidth: 1.5,
-                  borderBottomWidth: 3,
-                  borderRightWidth: 3,
-                  borderRadius: 24,
-                }}
-              >
-                <Text style={styles.btnText}>확인</Text>
-              </View>
-            </TouchableOpacity>
+            <View style={{ marginTop: 30, marginBottom: 30 }}>
+              <TouchableOpacity onPress={onCancel} activeOpacity={0.6}>
+                <RegularBtnOrange text={"확인"} />
+              </TouchableOpacity>
+            </View>
           </View>
         </View>
       </View>
@@ -118,4 +118,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default ReportMsg;
+export default ReportResult;
