ada1ee33802031dc33c4a29e1f8c42e6e2865a71
KwonSuyeon <commetoi_yeoni@naver.com>
2024-02-12 17:46:23 +0900

    fix: 절약 목표 없을 때 ui 수정

--- components/Top.jsx
+++ components/Top.jsx
@@ -19,22 +19,25 @@ const Top = ({ navigation }) => {
   const [day, setDay] = useState(0);
   const [progress, setProgress] = useState(0);
   const progressAnim = useRef(new Animated.Value(0)).current; // 초기 값 0으로 설정
-
+  const [saveData, setSaveData] = useState(false);
   // 절약 목표 조회
   const fetchData = async () => {
     const access_token = await AsyncStorage.getItem("access_token");
     try {
-      const response = await axios.post(url, {
+      const response = await axios.get(url, {
         headers: {
           "Content-Type": "application/json; charset=utf-8",
           Authorization: `Bearer ${access_token}`,
         },
       });
       if (response.status === 200) {
-        const SaveData = response.data.data;
+        const SaveData = response.data;
         console.log("절약 목표 조회::", SaveData);
+        setSaveData(true);
         setDay(SaveData.date);
         setProgress(SaveData.savePercentage);
+      } else if (response.status === 404) {
+        setSaveData(false);
       }
     } catch (error) {
       console.error("에러:", error);
@@ -63,7 +66,11 @@ const Top = ({ navigation }) => {
   return (
     <View style={styles.container}>
       <View style={styles.progressContainer}>
-        <Text style={styles.text}>D - {day}</Text>
+        {saveData ? (
+          <Text style={styles.text}>D - {day}</Text>
+        ) : (
+          <Text style={styles.text}>D - ??</Text>
+        )}
         <TouchableOpacity onPress={() => navigation.navigate("Goal")}>
           <View style={styles.progressBar}>
             <Animated.View
@@ -74,17 +81,30 @@ const Top = ({ navigation }) => {
                 },
               ]}
             />
-            <Text
-              style={{
-                marginLeft: -20,
-                fontSize: 16,
-                fontWeight: 900,
-                color: "#1F1F1F",
-                transform: [{ translateY: 0 }, { translateX: 90 }],
-              }}
-            >
-              {progress}%
-            </Text>
+            {saveData ? (
+              <Text
+                style={{
+                  marginLeft: -20,
+                  fontSize: 16,
+                  fontWeight: 900,
+                  color: "#1F1F1F",
+                  transform: [{ translateY: 0 }, { translateX: 90 }],
+                }}
+              >
+                {progress}%
+              </Text>
+            ) : (
+              <Text
+                style={{
+                  fontSize: 16,
+                  fontWeight: 900,
+                  color: "#1F1F1F",
+                  transform: [{ translateY: 0 }, { translateX: 15 }],
+                }}
+              >
+                절약 목표를 설정해보세요!
+              </Text>
+            )}
           </View>
         </TouchableOpacity>
         <TouchableOpacity onPress={() => navigation.navigate("Login")}>
@@ -146,7 +166,7 @@ const styles = StyleSheet.create({
   },
   progressBar: {
     position: "relative",
-    width: 175,
+    width: 200,
     height: 24,
     borderRadius: 29,
     borderWidth: 1.5,
