0c7afa0d8699d2d6bb01b93cd0d3e4b9eec08072
KwonSuyeon <commetoi_yeoni@naver.com>
2024-02-18 23:53:25 +0900

    feat: 검색 결과 기능 추가

--- screen/Community/search.jsx
+++ screen/Community/search.jsx
@@ -8,13 +8,19 @@ import {
   StyleSheet,
   ScrollView,
 } from "react-native";
+import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import Line from "../../components/Line";
+import { proxyUrl } from "../../constant/common";
 
 const Search = () => {
+  const inputURL = "/community/board";
+  const url = proxyUrl + inputURL;
+
   const RECENT_SEARCHES_KEY = "recent_searches";
   const [text, setText] = useState("");
   const [recentSearches, setRecentSearches] = useState([]);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     const loadRecentSearches = async () => {
@@ -24,7 +30,7 @@ const Search = () => {
           setRecentSearches(JSON.parse(savedSearches));
         }
       } catch (e) {
-        // Error loading searches
+        console.error("Error loading recent searches", e);
       }
     };
 
@@ -36,7 +42,7 @@ const Search = () => {
       const jsonValue = JSON.stringify(searches);
       await AsyncStorage.setItem(RECENT_SEARCHES_KEY, jsonValue);
     } catch (e) {
-      // error
+      console.error("Error saving recent searches", e);
     }
   };
 
@@ -45,6 +51,7 @@ const Search = () => {
   };
 
   const handleSearch = () => {
+    fetchData();
     const newSearches = [
       text,
       ...recentSearches.filter((search) => search !== text),
@@ -65,6 +72,35 @@ const Search = () => {
     saveRecentSearches(updatedSearches);
   };
 
+  const fetchData = async () => {
+    const access_token = await AsyncStorage.getItem("access_token");
+
+    try {
+      const paramsData = {
+        sortType: "LATEST",
+        pageSize: 20,
+        search: text,
+      };
+      const response = await axios.get(url, {
+        headers: {
+          "Content-Type": "application/json; charset=utf-8",
+          Authorization: `Bearer ${access_token}`,
+        },
+        params: paramsData,
+      });
+
+      // Check if data exists and is an array
+      if (response.data && Array.isArray(response.data.data)) {
+        setData(response.data.data);
+      } else {
+        setData([]); // Set data to an empty array if response is not as expected
+      }
+    } catch (error) {
+      console.error("Search error:", error);
+      setData([]); // Reset data in case of error
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.contentContainer}>
@@ -72,7 +108,7 @@ const Search = () => {
           <View style={styles.keyboard}>
             <TextInput
               placeholderTextColor="#C1C1C1"
-              placeholder={"원하는 글, 검색어를 찾아보세요"}
+              placeholder="원하는 글, 검색어를 찾아보세요"
               value={text}
               onChangeText={handleInputChange}
               style={styles.input}
@@ -89,51 +125,78 @@ const Search = () => {
             </TouchableOpacity>
           </View>
         </View>
-        <View style={styles.recentSearchesHeader}>
-          <Text style={styles.text}>최근 검색어</Text>
+        {/* 데이터 있을 떄만 */}
+        {data.length > 0 ? (
+          <View style={styles.recentSearchesHeader}></View>
+        ) : (
+          <View style={styles.recentSearchesHeader}>
+            <Text style={styles.text}>최근 검색어</Text>
 
-          <View style={{ flexDirection: "row", gap: 7 }}>
-            <TouchableOpacity onPress={clearRecentSearches}>
-              <Text style={styles.text}>전체삭제</Text>
-            </TouchableOpacity>
-            <View
-              style={{ backgroundColor: "#C1C1C1", width: 1, height: 14 }}
-            />
-            <Text style={styles.text}>자동저장끄기</Text>
-          </View>
-        </View>
-        <ScrollView>
-          {recentSearches.map((search, index) => (
-            <View
-              style={{
-                backgroundColor: "#fff",
-              }}
-            >
+            <View style={{ flexDirection: "row", gap: 7 }}>
+              <TouchableOpacity onPress={clearRecentSearches}>
+                <Text style={styles.text}>전체삭제</Text>
+              </TouchableOpacity>
               <View
-                style={{
-                  display: "flex",
-                  flexDirection: "row",
-                  justifyContent: "space-between",
-                  paddingRight: 20,
-                  paddingLeft: 20,
-                  paddingBottom: 16,
-                  paddingTop: 16,
-                }}
-              >
-                <Text key={index} style={styles.recentSearch}>
-                  {search}
-                </Text>
-                <TouchableOpacity onPress={() => removeSearchTerm(index)}>
-                  <Image
-                    source={require("../../assets/grayClose.png")}
-                    style={{ width: 24, height: 24 }}
-                  />
-                </TouchableOpacity>
-              </View>
-              <Line h={1} color={"#C1C1C1"} />
+                style={{ backgroundColor: "#C1C1C1", width: 1, height: 14 }}
+              />
+              <Text style={styles.text}>자동저장끄기</Text>
             </View>
-          ))}
-        </ScrollView>
+          </View>
+        )}
+        {data.length > 0 ? (
+          <ScrollView>
+            {data.map((item, index) => (
+              <View key={index} style={{ backgroundColor: "#fff" }}>
+                <View
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    paddingRight: 20,
+                    paddingLeft: 20,
+                    paddingBottom: 16,
+                    paddingTop: 16,
+                  }}
+                >
+                  <Text>{item.type}</Text>
+                  <Text>{item.title}</Text>
+                  <Text>{item.writer}</Text>
+                  <Text>{item.createdAt}</Text>
+                  <Text>조회 {item.viewCount}</Text>
+                  <Text>댓글 {item.commentCount}</Text>
+                </View>
+                <Line h={1} color={"#C1C1C1"} />
+              </View>
+            ))}
+          </ScrollView>
+        ) : recentSearches.length > 0 ? (
+          <ScrollView>
+            {recentSearches.map((search, index) => (
+              <View key={index} style={{ backgroundColor: "#fff" }}>
+                <View
+                  style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    paddingRight: 20,
+                    paddingLeft: 20,
+                    paddingBottom: 16,
+                    paddingTop: 16,
+                  }}
+                >
+                  <Text style={styles.recentSearch}>{search}</Text>
+                  <TouchableOpacity onPress={() => removeSearchTerm(index)}>
+                    <Image
+                      source={require("../../assets/grayClose.png")}
+                      style={{ width: 24, height: 24 }}
+                    />
+                  </TouchableOpacity>
+                </View>
+                <Line h={1} color={"#C1C1C1"} />
+              </View>
+            ))}
+          </ScrollView>
+        ) : null}
       </View>
     </View>
   );
