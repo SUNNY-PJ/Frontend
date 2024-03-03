b53c8457adb0e40427444f2bd97f6671de94eb4c
yeoniii20 <commetoi_yeoni@naver.com>
2024-03-03 23:36:42 +0900

    feat: xCode setting and add

---
+++ SUNNY/SUNNY/SUNNYApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUNNYApp.swift
+//  SUNNY
+//
+//  Created by 권수연 on 3/3/24.
+//
+
+import SwiftUI
+
+@main
+struct SUNNYApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
