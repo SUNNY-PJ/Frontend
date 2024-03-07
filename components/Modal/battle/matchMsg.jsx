9a9d3a901be6ee8b967957670b2b855ea37683f6
yeoniii20 <commetoi_yeoni@naver.com>
2024-03-07 22:34:34 +0900

    feat: 대결 신청 수락/거절 기능 추가

--- components/Modal/battle/matchMsg.jsx
+++ components/Modal/battle/matchMsg.jsx
@@ -9,8 +9,96 @@ import {
 } from "react-native";
 import MiddleBtn from "../../Btn/middleBtn";
 import MiddleBtnBlack from "../../Btn/middleBtnBlack";
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { proxyUrl } from "../../constant/common";
 
-const MatchMsg = ({ isVisible, toggleModal }) => {
+const MatchMsg = ({ isVisible, toggleModal, friendsId }) => {
+  const inputURL = `/competition/approve/${friendsId}`;
+  const url = proxyUrl + inputURL;
+
+  // 대결 신청 수락
+  const approveMatch = async () => {
+    const access_token = await AsyncStorage.getItem("access_token");
+    try {
+      const response = await axios.post(
+        url,
+        {},
+        {
+          headers: {
+            "Content-Type": "application/json; charset=utf-8",
+            Authorization: `Bearer ${access_token}`,
+          },
+        }
+      );
+
+      console.log(response.data);
+      if (response.status === 200) {
+        if (response.data.status === 400) {
+          Alert.alert(
+            "Error",
+            `서버 장애가 발생했습니다.\n관리자에게 문의 바랍니다.`
+          );
+        } else {
+          Alert.alert("대결 수락", `대결 신청을 수락했습니다.`);
+          // navigation.navigate("MainScreen", { screen: "FriendsList" });
+        }
+      }
+    } catch (error) {
+      if (error.response) {
+        console.error("서버 응답 오류:", error.response.data);
+        console.error("서버 응답 메세지:", error.message);
+      } else {
+        console.error("에러:", error);
+      }
+    }
+  };
+
+  // 대결 신청 거절
+  const refuseMatch = async () => {
+    const access_token = await AsyncStorage.getItem("access_token");
+    try {
+      const response = await axios.delete(
+        url,
+        {},
+        {
+          headers: {
+            "Content-Type": "application/json; charset=utf-8",
+            Authorization: `Bearer ${access_token}`,
+          },
+        }
+      );
+
+      console.log(response.data);
+      if (response.status === 200) {
+        if (response.data.status === 400) {
+          Alert.alert(
+            "Error",
+            `서버 장애가 발생했습니다.\n관리자에게 문의 바랍니다.`
+          );
+        } else {
+          Alert.alert("대결 거절", `대결 신청을 거절했습니다.`);
+          // navigation.navigate("MainScreen", { screen: "FriendsList" });
+        }
+      }
+    } catch (error) {
+      if (error.response) {
+        console.error("서버 응답 오류:", error.response.data);
+        console.error("서버 응답 메세지:", error.message);
+      } else {
+        console.error("에러:", error);
+      }
+    }
+  };
+
+  const handleApprove = () => {
+    approveMatch();
+  };
+
+  const handleRefuse = () => {
+    refuseMatch();
+  };
+
   return (
     <Modal animationType="none" transparent={true} visible={isVisible}>
       <View
@@ -50,8 +138,12 @@ const MatchMsg = ({ isVisible, toggleModal }) => {
             </View>
           </View>
           <View style={styles.buttonContainer}>
-            <MiddleBtn text={"거절"} />
-            <MiddleBtnBlack text={"승낙"} />
+            <TouchableOpacity onPress={handleRefuse}>
+              <MiddleBtn text={"거절"} />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={handleApprove}>
+              <MiddleBtnBlack text={"승낙"} />
+            </TouchableOpacity>
           </View>
         </View>
       </View>
