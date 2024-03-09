779a36ae60433eda2112e704c79a10b67e1a1dbe
yeoniii20 <commetoi_yeoni@naver.com>
2024-03-10 03:35:31 +0900

    update: xcode and app.json

--- SUNNY/SUNNYTests/SUNNYTests.swift
+++ SUNNY/SUNNYTests/SUNNYTests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+@testable import SUNNY
 
 final class SUNNYTests: XCTestCase {
 
@@ -27,7 +28,7 @@ final class SUNNYTests: XCTestCase {
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        measure {
+        self.measure {
             // Put the code you want to measure the time of here.
         }
     }
