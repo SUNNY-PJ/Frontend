8d91bfe2af82d893d602b59223ba079548bb8942
yeoniii20 <commetoi_yeoni@naver.com>
2024-03-21 00:09:47 +0900

    update: donutChart

--- components/Chart/donutChart.jsx
+++ components/Chart/donutChart.jsx
@@ -4,40 +4,54 @@ import Svg, { Circle, Path } from "react-native-svg";
 
 const DonutChart = ({ data, onCategorySelect }) => {
   // 도넛 차트의 중심 좌표 및 반지름 설정
-  const centerX = 110; // 중앙 X 좌표를 수정합니다.
-  const centerY = 110; // 중앙 Y 좌표를 수정합니다.
-  const radius = 100; // 반지름을 수정합니다.
-  const strokeWidth = 110; // 선의 두께를 조정합니다.
-  const textOffset = 20; // 텍스트의 수직 오프셋을 조정합니다.
-  const textDistance = 15; // 텍스트의 수평 거리를 조정합니다.
+  const centerX = 110; // 중앙 X 좌표를 수정
+  const centerY = 110; // 중앙 Y 좌표를 수정
+  const radius = 100; // 반지름을 수정
+  const strokeWidth = 110; // 선의 두께를 조정
+  const textOffset = 20; // 텍스트의 수직 오프셋을 조정
+  const textDistance = 15; // 텍스트의 수평 거리를 조정
 
   // 각도 계산
   const total = data.reduce((acc, curr) => acc + curr.value, 0);
   let startAngle = -90;
   let prevTextX = 0;
 
-  const handlePress = (item) => {
-    console.log("Clicked data:", item);
-  };
+  const allZero = data.every((item) => item.value === 0);
+  const adjustedData = allZero
+    ? data.map((item) => ({ ...item, value: 100 / data.length }))
+    : data;
 
-  const handleCategoryClick = () => {};
+  //   <Circle
+  //   cx={centerX}
+  //   cy={centerY}
+  //   r={radius}
+  //   fill="#FFFBF6"
+  //   stroke="black"
+  //   strokeWidth="1.5"
+  // />
 
   return (
     <View style={styles.container}>
       <Svg width="224" height="224">
         {data.flatMap((item, index) => {
-          const endAngle = (item.value / total) * 360 + startAngle;
-          const largeArcFlag = item.value / total > 0.5 ? 1 : 0;
-          const path = `
-            M ${centerX},${centerY}
-            L ${centerX + radius * Math.cos((startAngle * Math.PI) / 180)},${
-            centerY + radius * Math.sin((startAngle * Math.PI) / 180)
+          const valuePercentage = allZero ? 100 : (item.value / total) * 100;
+          const endAngle = (valuePercentage / 100) * 360 + startAngle;
+          const largeArcFlag = valuePercentage > 50 ? 1 : 0;
+
+          if (isNaN(endAngle)) {
+            console.error("Invalid end angle:", endAngle);
+            return null;
           }
-            A ${radius},${radius} 0 ${largeArcFlag},1 ${
-            centerX + radius * Math.cos((endAngle * Math.PI) / 180)
-          },${centerY + radius * Math.sin((endAngle * Math.PI) / 180)}
-            Z
-          `;
+
+          const path = `
+      M ${centerX},${centerY}
+      L ${centerX + radius * Math.cos((Math.PI * startAngle) / 180)},
+        ${centerY + radius * Math.sin((Math.PI * startAngle) / 180)}
+      A ${radius},${radius} 0 ${largeArcFlag},1 
+        ${centerX + radius * Math.cos((Math.PI * endAngle) / 180)},
+        ${centerY + radius * Math.sin((Math.PI * endAngle) / 180)}
+      Z
+    `;
 
           // 텍스트 위치 계산
           const textAngle = (startAngle + endAngle) / 2; // 중간 각도
@@ -48,7 +62,7 @@ const DonutChart = ({ data, onCategorySelect }) => {
           let textY =
             centerY +
             (radius - strokeWidth / 2 + textOffset) *
-              Math.sin((textAngle * Math.PI) / 180); // 텍스트의 Y 좌표
+              Math.sin((textAngle * Math.PI) / 180); // 텍스트의 Y 좌표ㅁ
 
           // 이전 텍스트와 겹치지 않도록 조절
           if (textX - prevTextX < textDistance) {
@@ -66,7 +80,7 @@ const DonutChart = ({ data, onCategorySelect }) => {
             <Path
               key={`${index}_path`}
               d={path}
-              fill={item.color}
+              fill={allZero ? "#FFFBF6" : item.color}
               stroke="black"
               strokeWidth="1.5"
               onPress={() => onCategorySelect(item.category)}
@@ -78,7 +92,7 @@ const DonutChart = ({ data, onCategorySelect }) => {
           cx={centerX}
           cy={centerY}
           r={radius - strokeWidth / 2}
-          fill="white"
+          fill="#FFFBF6"
           stroke="black"
           strokeWidth="1.5"
           onPress={() => onCategorySelect("center")}
@@ -90,6 +104,8 @@ const DonutChart = ({ data, onCategorySelect }) => {
             style={{
               gap: 3,
               marginTop: 10,
+              alignItems: "center",
+              width: 50,
             }}
           >
             <View
@@ -116,7 +132,6 @@ const styles = StyleSheet.create({
     height: 36,
     justifyContent: "center",
     alignItems: "center",
-    // backgroundColor: "#5C5C5C",
     paddingTop: 7,
     paddingBottom: 7,
     paddingRight: 5,
@@ -132,8 +147,38 @@ const styles = StyleSheet.create({
   text: {
     fontFamily: "SUITE_Bold",
     color: "#1F1F1F",
-    alignSelf: "center",
   },
 });
 
 export default DonutChart;
+
+// const donutData = [
+//   {
+//     value: 29,
+//     color: "#007560",
+//     url: require("../../assets/clothes_chart.png"),
+//     title: "의류",
+//     category: "CLOTHING",
+//   },
+//   {
+//     value: 20,
+//     color: "#6adca3",
+//     url: require("../../assets/food_chart.png"),
+//     title: "식생활",
+//     category: "FOOD",
+//   },
+//   {
+//     value: 12,
+//     color: "#b9f4d6",
+//     url: require("../../assets/home_chart.png"),
+//     title: "주거",
+//     category: "SHELTER",
+//   },
+//   {
+//     value: 38,
+//     color: "#e9fbf2",
+//     url: require("../../assets/ect_chart.png"),
+//     title: "기타",
+//     category: "OTHERS",
+//   },
+// ];
