21fe0b261c4eb961e03a77e06701aaf347135d6d
yeoniii20 <commetoi_yeoni@naver.com>
2024-04-02 00:13:36 +0900

    feat: communityBottomSheet 생성

---
+++ components/BottomSheet/communitySheet.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { View, Text, Modal, TouchableOpacity, StyleSheet } from "react-native";
+import Line from "../Line";
+
+const CommunitySheet = ({ isVisible, onClose, onProfile, onReport }) => {
+  return (
+    <Modal
+      animationType="none"
+      transparent={true}
+      visible={isVisible}
+      onRequestClose={onClose}
+    >
+      <TouchableOpacity style={styles.overlay} onPress={onClose}>
+        <View style={{ padding: 17 }}>
+          <View style={styles.actionSheet}>
+            <TouchableOpacity style={styles.actionItem} onPress={onProfile}>
+              <Text style={styles.actionText}>작성자 프로필 보기</Text>
+            </TouchableOpacity>
+            <Line h={1} color={"#C1C1C1"} />
+            <TouchableOpacity style={styles.actionItem} onPress={onReport}>
+              <Text style={styles.actionText}>게시글 신고하기</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={[styles.actionSheet, { marginTop: 5 }]}>
+            <TouchableOpacity style={styles.actionItem} onPress={onClose}>
+              <Text style={styles.actionText}>취소</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </TouchableOpacity>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  overlay: {
+    flex: 1,
+    justifyContent: "flex-end",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  actionSheet: {
+    backgroundColor: "white",
+
+    alignItems: "center",
+    borderRadius: 16,
+  },
+  actionItem: {
+    paddingVertical: 12,
+    width: "100%",
+    alignItems: "center",
+  },
+  actionText: {
+    fontSize: 20,
+    fontWeight: 500,
+    color: "#1F1F1F",
+    paddingTop: 10,
+    paddingBottom: 10,
+    fontFamily: "SUITE",
+  },
+});
+
+export default CommunitySheet;
