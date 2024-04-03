717a2c1c4f1db2415a208b7e914849f29ad7ca50
yeoniii20 <commetoi_yeoni@naver.com>
2024-04-04 01:29:13 +0900

    update: login 여부에 따른 화면 랜더링

--- Navigation.js
+++ Navigation.js
@@ -10,32 +10,41 @@ import { SaveDataProvider } from "./context/saveDataContext";
 
 const Stack = createStackNavigator();
 
-function Navigation() {
+function Navigation({ isSignedIn }) {
   return (
     <CommunityProvider>
       <SaveDataProvider>
         <NavigationContainer>
           <Stack.Navigator>
-            <Stack.Screen
-              name="KakaoScreen"
-              component={KakaoScreen}
-              options={{ headerShown: false }}
-            />
-            <Stack.Screen
-              name="SignUpScreen"
-              component={SignUpScreen}
-              options={{ headerShown: false }}
-            />
-            <Stack.Screen
-              name="MainScreen"
-              component={MainScreen}
-              options={{ headerShown: false }}
-            />
-            <Stack.Screen
+            {isSignedIn ? (
+              <>
+                {/* 로그인 상태 */}
+                <Stack.Screen
+                  name="MainScreen"
+                  component={MainScreen}
+                  options={{ headerShown: false }}
+                />
+              </>
+            ) : (
+              <>
+                {/* 비로그인 상태 */}
+                <Stack.Screen
+                  name="KakaoScreen"
+                  component={KakaoScreen}
+                  options={{ headerShown: false }}
+                />
+                <Stack.Screen
+                  name="SignUpScreen"
+                  component={SignUpScreen}
+                  options={{ headerShown: false }}
+                />
+              </>
+            )}
+            {/* <Stack.Screen
               name="ChatScreen"
               component={ChatScreen}
               options={{ headerShown: false }}
-            />
+            /> */}
           </Stack.Navigator>
         </NavigationContainer>
       </SaveDataProvider>
