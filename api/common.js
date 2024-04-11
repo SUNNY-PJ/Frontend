0cb51c2f2d0748a0e3f094717ea1cf8b5970d20d
yeoniii20 <commetoi_yeoni@naver.com>
2024-04-12 00:06:10 +0900

    fix: gotIgnore 수정

---
+++ api/common.js
@@ -0,0 +1,2 @@
+export const proxyUrl = "http://43.201.176.22:8080";
+export const proxy_url = "43.201.176.22:8080";
---
+++ constant/common.js
@@ -0,0 +1,3 @@
+export const proxyUrl = "http://43.201.176.22:8080";
+export const proxy_url = "43.201.176.22:8080";
+export const stompUrl = "http://localhost:8080/stomp";
