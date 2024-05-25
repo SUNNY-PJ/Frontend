af03bfe7799f3223d8024789f8801a2b61b5e957
yeoniii20 <commetoi_yeoni@naver.com>
2024-05-25 23:43:15 +0900

    refactor: modify styles 코드 분리

---
+++ screen/Community/modify.styles.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flexDirection: "column",
+    backgroundColor: "#FFFBF6",
+    minHeight: "100%",
+  },
+  contentContainer: {
+    paddingLeft: 20,
+    paddingRight: 21,
+  },
+  media: {
+    flexDirection: "row",
+    gap: 16,
+  },
+  text: {
+    fontSize: 16,
+    color: "#1F1F1F",
+    fontFamily: "SUITE_Medium",
+  },
+});
