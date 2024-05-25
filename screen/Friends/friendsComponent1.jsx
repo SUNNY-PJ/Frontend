6cbd9f24eadca5ad9192734f26a0e6cbac60e49e
yeoniii20 <commetoi_yeoni@naver.com>
2024-05-26 01:20:41 +0900

    refactor: friendsComponents styles 코드 분리

--- screen/Friends/friendsComponent1.jsx
+++ screen/Friends/friendsComponent1.jsx
@@ -1,12 +1,6 @@
 import React, { useState, useEffect } from "react";
-import {
-  View,
-  Text,
-  Image,
-  StyleSheet,
-  TouchableOpacity,
-  ScrollView,
-} from "react-native";
+import { View, Text, Image, TouchableOpacity, ScrollView } from "react-native";
+import { styles } from "./friendsComponent.styles";
 import Line from "../../components/Line";
 import { useNavigation } from "@react-navigation/native";
 import FriendProfile from "./friendProfile";
@@ -41,19 +35,7 @@ const FriendsComponent1 = ({ Data, onAddFriend, onRemoveFriend }) => {
   return (
     <ScrollView>
       {Data.map((item, index) => (
-        <View
-          key={index}
-          style={{
-            flexDirection: "row",
-            justifyContent: "space-between",
-            paddingLeft: 28,
-            paddingRight: 28,
-            paddingBottom: 20,
-            paddingTop: 20,
-            borderBottomColor: "#C1C1C1",
-            borderBottomWidth: 1,
-          }}
-        >
+        <View key={index} style={styles.container}>
           <TouchableOpacity
             activeOpacity={0.6}
             style={{ flexDirection: "row" }}
@@ -62,17 +44,7 @@ const FriendsComponent1 = ({ Data, onAddFriend, onRemoveFriend }) => {
             }}
           >
             <Image source={{ uri: item.profile }} style={styles.icon} />
-            <Text
-              style={{
-                marginLeft: 8,
-                fontSize: 16,
-                color: "#1F1F1F",
-                alignSelf: "center",
-                fontFamily: "SUITE_Medium",
-              }}
-            >
-              {item.nickname}
-            </Text>
+            <Text style={styles.nickname}>{item.nickname}</Text>
           </TouchableOpacity>
           {/* //     NONE, PENDING, PROCEEDING, GIVE_UP, COMPLETE; */}
           {/* //   WIN("승리"), LOSE("패배"),DRAW("무승부"); */}
@@ -112,53 +84,5 @@ const FriendsComponent1 = ({ Data, onAddFriend, onRemoveFriend }) => {
     </ScrollView>
   );
 };
-const styles = StyleSheet.create({
-  titleSection: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    paddingLeft: 20,
-    paddingRight: 20,
-    marginBottom: 8,
-  },
-  mainTitle: {
-    fontSize: 22,
-    color: "#1F1F1F",
-    textAlign: "center",
-    marginBottom: 24,
-    fontFamily: "SUITE_Bold",
-  },
-  title: {
-    fontSize: 20,
-    fontFamily: "SUITE_Bold",
-    color: "#1F1F1F",
-  },
-  menuItem: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  icon: {
-    height: 40,
-    width: 40,
-    borderRadius: 50,
-  },
-  deleteBox: {
-    backgroundColor: "#5C5C5C",
-    justifyContent: "center",
-    alignItems: "center",
-    width: "100%",
-    height: "100%",
-    paddingLeft: 20,
-    paddingRight: 20,
-    marginLeft: 10,
-    marginRight: 10,
-    borderBottomColor: "#C1C1C1",
-    borderBottomWidth: 1,
-  },
-  deleteButtonText: {
-    color: "#fff",
-    fontFamily: "SUITE",
-  },
-});
 
 export default FriendsComponent1;
