eb1dcface9a2510bf744dd32361ceadebc064a9a
yeoniii20 <commetoi_yeoni@naver.com>
2024-05-26 11:46:03 +0900

    refactor: battle msg styles 코드 분리

---
+++ screen/Battle/sendMatch.styles.js
@@ -0,0 +1,40 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#FFFBF6",
+    height: "100%",
+  },
+  contentContainer: {
+    marginTop: 25,
+    marginBottom: 40,
+    paddingLeft: 28,
+    paddingRight: 27,
+  },
+  headerText: {
+    fontSize: 22,
+    color: "#1F1F1F",
+    textAlign: "center",
+    fontFamily: "SUITE_Bold",
+    marginBottom: 8,
+  },
+  label: {
+    fontSize: 16,
+    color: "#1F1F1F",
+    marginBottom: 8,
+    marginTop: 24,
+    paddingLeft: 10,
+    fontFamily: "SUITE_Medium",
+  },
+
+  buttonContainer: {
+    marginTop: 80,
+  },
+  subText: {
+    fontSize: 12,
+    color: "#5C5C5C",
+    paddingLeft: 10,
+    fontFamily: "SUITE",
+    marginTop: 8,
+  },
+});
