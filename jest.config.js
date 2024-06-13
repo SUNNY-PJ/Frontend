972030a681d69b7e0681f7bfb30e078f5f4f58ab
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-06-13 15:26:25 +0900

    fix: update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,8 @@ module.exports = {
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
-    '^@react-native-async-storage/async-storage$': '<rootDir>/__mocks__/async-storage.js'
+    '^@react-native-async-storage/async-storage$': '<rootDir>/__mocks__/async-storage.js',
+    '^expo-notifications$': '<rootDir>/__mocks__/expo-notifications.js',
   },
   transformIgnorePatterns: [
     "node_modules/(?!(@react-native|react-native|react-native-.*|@react-navigation|expo-.*)/)"
