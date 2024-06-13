c82779c36d2f30a851d8515c5afcdfbdce3085e8
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-06-13 14:40:01 +0900

    feat: create App.test.js

---
+++ __tests__/App.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from '@testing-library/react-native';
+import App from '../App';
+
+test('renders correctly', () => {
+  const { getByText } = render(<App />);
+  expect(getByText('Welcome to React Native!')).toBeTruthy();
+});
