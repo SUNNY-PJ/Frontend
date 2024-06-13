815cefdaa7232b5e07a0158957a682d0122dce44
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-06-13 15:23:17 +0900

    feat: create jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  preset: 'react-native',
+  testEnvironment: 'node',
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
+    '^@react-native-async-storage/async-storage$': '<rootDir>/__mocks__/async-storage.js'
+  },
+  transformIgnorePatterns: [
+    "node_modules/(?!(@react-native|react-native|react-native-.*|@react-navigation|expo-.*)/)"
+  ],
+};
