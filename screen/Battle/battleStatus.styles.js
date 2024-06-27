8863020f37269efe87c6522e5c3386a861eebd2b
yeoniii20 <commetoi_yeoni@naver.com>
2024-06-28 02:30:51 +0900

    style: change battleStatus screen style

--- screen/Battle/battleStatus.styles.js
+++ screen/Battle/battleStatus.styles.js
@@ -1,30 +1,21 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "#FFFBF6",
-    height: "100%",
-  },
+  container: { backgroundColor: "#FFFBF6", height: "100%" },
   section: {
     paddingBottom: 14,
     backgroundColor: "#fff",
     alignItems: "center",
   },
-  boldText: {
-    fontSize: 22,
-    color: "#C1C1C1",
-    fontFamily: "SUITE_ExtraBold",
-  },
+  boldText: { fontSize: 22, color: "#C1C1C1", fontFamily: "SUITE_ExtraBold" },
+  boldText2: { fontSize: 22, color: "#1F1F1F", fontFamily: "SUITE_ExtraBold" },
   boldSmallText: {
     fontSize: 16,
     color: "#1F1F1F",
     marginTop: 14,
     fontFamily: "SUITE_ExtraBold",
   },
-  text: {
-    fontSize: 20,
-    fontFamily: "SUITE",
-  },
+  text: { fontSize: 20, fontFamily: "SUITE" },
   subText: {
     fontSize: 16,
     color: "#5C5C5C",
