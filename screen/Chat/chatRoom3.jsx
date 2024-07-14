1dc79f628958b05a28bd1d6f51f890af8f7ffeb3
yeoniii20 <commetoi_yeoni@naver.com>
2024-07-14 15:24:49 +0900

    refactor: styles 파일 분리

--- screen/Chat/chatRoom3.jsx
+++ screen/Chat/chatRoom3.jsx
@@ -4,7 +4,6 @@ import {
   Text,
   TextInput,
   ScrollView,
-  StyleSheet,
   KeyboardAvoidingView,
   Platform,
   Image,
@@ -16,6 +15,7 @@ import { useNavigation } from "@react-navigation/native";
 import { Client } from "@stomp/stompjs";
 import Line from "../../components/Line";
 import { SOCKET_URI } from "../../api/common";
+import styles from "./chatRoom3.styels";
 
 const ChatRoom3 = () => {
   const navigation = useNavigation();
@@ -42,6 +42,7 @@ const ChatRoom3 = () => {
 
       const chatData = response.data;
       console.log("대화 내용 :::", chatData);
+      setReceivedMessages(chatData);
     } catch (error) {
       console.error("에러:", error);
     }
@@ -164,38 +165,9 @@ const ChatRoom3 = () => {
     >
       <View style={styles.container}>
         <ScrollView style={[styles.messagesContainer]} ref={scrollViewRef}>
-          <View
-            style={{
-              alignItems: "center",
-              gap: 16,
-              paddingBottom: 60,
-              paddingTop: 68,
-            }}
-          >
-            <View
-              style={{
-                backgroundColor: "#6ADCA3",
-                borderWidth: 3,
-                borderBottomWidth: 4.5,
-                borderRightWidth: 4.5,
-                borderColor: "#1F1F1F",
-                borderRadius: 55,
-                paddingBottom: 9,
-                paddingTop: 9,
-                paddingRight: 18,
-                paddingLeft: 18,
-              }}
-            >
-              <Text
-                style={{
-                  fontSize: 15,
-                  fontWeight: 700,
-                  color: "#1F1F1F",
-                  fontFamily: "SUITE_Bold",
-                }}
-              >
-                Today
-              </Text>
+          <View style={styles.topSection}>
+            <View style={styles.dateSection}>
+              <Text style={styles.date}>Today</Text>
             </View>
             <View
               style={{
@@ -210,15 +182,7 @@ const ChatRoom3 = () => {
                   style={{ width: 24, height: 24, bottom: 11 }}
                 />
               </TouchableOpacity>
-              <Text
-                style={{
-                  flex: 1,
-                  fontSize: 12,
-                  fontWeight: 700,
-                  color: "#5C5C5C",
-                  textAlign: "center",
-                }}
-              >
+              <Text style={styles.noti}>
                 "친구가 아닌 사용자입니다. 친구를 맺을까요?"
               </Text>
               <Image
@@ -228,64 +192,56 @@ const ChatRoom3 = () => {
             </View>
           </View>
 
-          {receivedMessages.map((message, index) => (
-            <View key={index}>
-              {!message.isMine && (
-                <View style={{ flexDirection: "row", gap: 4 }}>
-                  <Image
-                    source={require("../../assets/Avatar.png")}
-                    style={{ width: 40, height: 40 }}
-                  />
-                  <Text
+          {receivedMessages.map((messageGroup, groupIndex) => (
+            <View key={groupIndex}>
+              <Text style={styles.date}>{messageGroup.createDate}</Text>
+              {messageGroup.messages.map((message, messageIndex) => (
+                <View key={messageIndex}>
+                  {!message.isMine && (
+                    <View style={{ flexDirection: "row", gap: 4 }}>
+                      <Image
+                        source={require("../../assets/Avatar.png")}
+                        style={{ width: 40, height: 40 }}
+                      />
+                      <Text style={styles.friendsName}>{message.nickname}</Text>
+                    </View>
+                  )}
+                  <View
                     style={{
-                      fontSize: 10,
-                      fontWeight: 700,
-                      color: "#1F1F1F",
-                      paddingTop: 3,
+                      flexDirection: "row",
+                      justifyContent: message.isMine
+                        ? "flex-end"
+                        : "flex-start",
                     }}
                   >
-                    민지
-                  </Text>
+                    {message.isMine && (
+                      <Text style={[styles.time, { marginRight: 8 }]}>
+                        {message.time}
+                      </Text>
+                    )}
+                    <View
+                      style={[
+                        message.isMine ? styles.message : styles.friendMessage,
+                        { marginBottom: 25 },
+                      ]}
+                    >
+                      <Text>{message.message}</Text>
+                    </View>
+                    {!message.isMine && (
+                      <Text
+                        style={[styles.time, { marginLeft: 8, bottom: 25 }]}
+                      >
+                        {message.time}
+                      </Text>
+                    )}
+                  </View>
                 </View>
-              )}
-              <View
-                style={{
-                  flexDirection: "row",
-                  justifyContent: message.isMine ? "flex-end" : "flex-start",
-                  // marginVertical: 5,
-                }}
-              >
-                {message.isMine && (
-                  <Text style={[styles.time, { marginRight: 8 }]}>
-                    {message.time}
-                  </Text>
-                )}
-                <View
-                  style={[
-                    message.isMine ? styles.message : styles.friendMessage,
-                    { marginBottom: 25 },
-                  ]}
-                >
-                  <Text>{message.text}</Text>
-                </View>
-                {!message.isMine && (
-                  <Text style={[styles.time, { marginLeft: 8, bottom: 25 }]}>
-                    {message.time}
-                  </Text>
-                )}
-              </View>
+              ))}
             </View>
           ))}
         </ScrollView>
         <Line h={1} color={"#C1C1C1"} />
-        <View
-          style={{
-            paddingLeft: 22,
-            paddingRight: 22,
-            paddingTop: 20,
-            backgroundColor: "#fff",
-          }}
-        >
+        <View style={styles.inputSection}>
           <View style={styles.inputContainer}>
             <TextInput
               style={styles.input}
@@ -305,73 +261,5 @@ const ChatRoom3 = () => {
     </KeyboardAvoidingView>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "space-between",
-    backgroundColor: "#FFFBF6",
-    // paddingTop: 68,
-  },
-  messagesContainer: {
-    flex: 1,
-    paddingRight: 20,
-    paddingLeft: 20,
-  },
-  message: {
-    padding: 10,
-    marginVertical: 5,
-    borderRadius: 9,
-    borderBottomRightRadius: 0,
-    borderWidth: 1,
-    borderColor: "#1F1F1F",
-    backgroundColor: "#fff",
-    flex: -1,
-    alignSelf: "flex-end",
-    maxWidth: "60%",
-  },
-  friendMessage: {
-    marginVertical: 5,
-    borderRadius: 9,
-    borderTopLeftRadius: 0,
-    borderWidth: 1,
-    borderColor: "#1F1F1F",
-    padding: 10,
-    backgroundColor: "#E8E9E8",
-    flex: -1, // 내용물에 따라 크기 조정
-    alignSelf: "flex-start", // 메시지를 왼쪽 정렬
-    maxWidth: "60%",
-    marginLeft: 49,
-    bottom: 25,
-  },
-  inputContainer: {
-    flexDirection: "row",
-    padding: 10,
-    borderColor: "#C1C1C1",
-    borderWidth: 1.5,
-    borderRadius: 8,
-    marginBottom: 19,
-    backgroundColor: "#fff",
-  },
-  input: {
-    flex: 1,
-    fontSize: 12,
-    fontWeight: 700,
-    color: "#5C5C5C",
-    // padding: 10,
-  },
-  icon: {
-    width: 20,
-    height: 21,
-    marginLeft: 10,
-  },
-  time: {
-    fontSize: 10,
-    fontWeight: 500,
-    color: "#C1C1C1",
-    alignSelf: "flex-end",
-    marginBottom: 25,
-  },
-});
 
 export default ChatRoom3;
