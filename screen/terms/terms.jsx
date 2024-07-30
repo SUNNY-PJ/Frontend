6b5db3c01f27e1da955db3ec7d86f275f9ce48b5
yeoniii20 <commetoi_yeoni@naver.com>
2024-07-31 00:39:56 +0900

    feat: sse test

--- screen/terms/terms.jsx
+++ screen/terms/terms.jsx
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { View, Image, Text, TouchableOpacity, ScrollView } from "react-native";
 import { styles } from "./terms.styles";
 import { useNavigation } from "@react-navigation/native";
 import { TermsData } from "../../data/termsData";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import apiClient from "../../api/apiClient";
 
 const Terms = () => {
   const navigation = useNavigation();
+
+  // sse connect
+  const sseConnect = async () => {
+    console.log("test123");
+    const url = "/api/sse/subscribe";
+    try {
+      console.log("test12345");
+
+      const response = await apiClient.get(url, {
+        headers: {
+          "Content-Type": "application/json; charset=utf-8",
+        },
+        // params: {},
+      });
+      console.log("sse connect test ::: 연결합니다....", response.data);
+    } catch (error) {
+      console.error("sse 에러:", error);
+    }
+  };
+
+  useEffect(() => {
+    console.log("test");
+    sseConnect();
+  }, []);
 
   return (
     <View style={styles.container}>
