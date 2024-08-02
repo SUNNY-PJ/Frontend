668899406ba50cb61c10116146b2f8a44c996a3a
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-08-02 13:36:39 +0900

    feat: 변수 정의 및 data setting

--- screen/Chat/chatRoom3.jsx
+++ screen/Chat/chatRoom3.jsx
@@ -19,56 +19,50 @@ import Line from "../../components/Line";
 import { DEV_SOCKET_URI, SOCKET_URI } from "../../api/common";
 import styles from "./chatRoom3.styles";
 import useStore from "../../store/store";
+import { formatDate } from "../../utils/dateUtils";
 
 const ChatRoom3 = () => {
   const navigation = useNavigation();
   const route = useRoute();
-  const { friendsId } = route.params;
-  console.log("친구 아이디다 :::", friendsId);
+  const scrollViewRef = useRef();
+
+  // 친구 아이디
+  const { chatRoomId } = route.params;
+  console.log("채팅방 아이디 :::", chatRoomId);
+
+  // 본인 아이디
   const profile = useStore((state) => state.profile);
+  const myId = profile.id;
+
   const [client, setClient] = useState(null);
   const [currentMessage, setCurrentMessage] = useState("");
   const [receivedMessages, setReceivedMessages] = useState([]);
-  const [accessToken, setAccessToken] = useState("");
-  const [roomId, setRoomId] = useState(2);
-  const [userIds, setUserIds] = useState(1);
-  const sendUserId = profile.id;
-  const scrollViewRef = useRef();
-  const myId = profile.id;
 
-  const formatDate = (dateString) => {
-    if (!dateString) return "";
-
-    const dateParts = dateString.split("-");
-    if (dateParts.length !== 3) return dateString;
-
-    const isoDateString = `${dateParts[0]}-${dateParts[1]}-${dateParts[2]}T00:00:00Z`;
-    const date = new Date(isoDateString);
-    const formattedDate = new Intl.DateTimeFormat("ko-KR", {
-      month: "2-digit",
-      day: "2-digit",
-    }).format(date);
-
-    const [month, day] = formattedDate.split(".");
-    return `${month}월 ${day}일`;
-  };
-
+  // 대화 내용 조회
   const fetchData = async () => {
-    const inputURL = `/chat/${roomId}`;
+    const inputURL = `/chat/${chatRoomId}`;
     try {
       const response = await apiClient.get(inputURL, {
         headers: {
           "Content-Type": "application/json; charset=utf-8",
         },
+        params: { userId: myId },
       });
 
       const chatData = response.data;
-      console.log("대화 내용 :::", chatData);
+      console.log("대화 내용 >>>", chatData);
 
       const updatedChatData = chatData.flatMap((group) => {
         const formattedDate = formatDate(group.createDate);
+        console.log("날짜 >>>", formattedDate);
         return group.messages.map((message) => ({
           ...message,
+          id: message.id,
+          userId: message.userId,
+          message: message.message,
+          nickname: message.nickname,
+          profile: message.profile,
+          time: message.time,
           isMine: message.userId === myId,
           formattedDate,
         }));
@@ -83,12 +77,12 @@ const ChatRoom3 = () => {
 
   useEffect(() => {
     fetchData();
-  }, [roomId]);
+  }, [chatRoomId]);
 
+  // 소캣 연결
   useEffect(() => {
     const initializeWebSocket = async () => {
       const token = await AsyncStorage.getItem("access_token");
-      setAccessToken(token);
 
       const client = new Client({
         brokerURL: `ws://${DEV_SOCKET_URI}/stomp`,
@@ -101,8 +95,8 @@ const ChatRoom3 = () => {
         reconnectDelay: 5000,
         onConnect: () => {
           console.log("Connected to the server");
-          if (roomId) {
-            client.subscribe(`/sub/room/${roomId}`, (message) => {
+          if (chatRoomId) {
+            client.subscribe(`/sub/room/${chatRoomId}`, (message) => {
               console.log("Received message:", message);
               try {
                 const parsedMessage = JSON.parse(message.body);
@@ -117,6 +111,7 @@ const ChatRoom3 = () => {
                     message: parsedMessage.message,
                     userId: parsedMessage.userId,
                     nickname: parsedMessage.nickname,
+                    profile: parsedMessage.profile,
                     time: parsedMessage.time,
                     isMine: parsedMessage.userId === myId,
                     formattedDate,
@@ -152,35 +147,36 @@ const ChatRoom3 = () => {
         client.deactivate();
       }
     };
-  }, [roomId]);
+  }, [chatRoomId]);
 
-  const createRoom = () => {
-    if (client && client.connected) {
-      client.publish({
-        destination: "/pub/chat/message/users",
-        body: JSON.stringify({
-          users: userIds,
-          sendUserId: sendUserId,
-          message: "새로운 채팅방이 생성되었습니다.",
-        }),
-      });
-      console.log(`Room creation message sent for users: ${userIds}`);
-    } else {
-      console.log("Client not connected");
-    }
-  };
+  // 이건 친구 프로필에서 채팅방 유무 확인하고 진행되어야하는 로직
+  // const createRoom = () => {
+  //   if (client && client.connected) {
+  //     client.publish({
+  //       destination: "/pub/chat/message/users",
+  //       body: JSON.stringify({
+  //         users: friendsId,
+  //         sendUserId: myId,
+  //         message: "새로운 채팅방이 생성되었습니다.",
+  //       }),
+  //     });
+  //     console.log(`Room creation message sent for users: ${friendsId}`);
+  //   } else {
+  //     console.log("Client not connected");
+  //   }
+  // };
 
-  useEffect(() => {
-    createRoom();
-  }, [client]);
+  // useEffect(() => {
+  //   createRoom();
+  // }, [client]);
 
   const sendMessage = () => {
-    if (client && client.connected && roomId) {
+    if (client && client.connected && chatRoomId) {
       client.publish({
         destination: "/pub/chat/message/room",
         body: JSON.stringify({
-          roomId: roomId,
-          sendUserId: sendUserId,
+          roomId: chatRoomId,
+          sendUserId: myId,
           message: currentMessage,
         }),
       });
@@ -188,7 +184,7 @@ const ChatRoom3 = () => {
       setCurrentMessage("");
       scrollToEnd();
     } else {
-      console.log("Client not connected or roomId not set");
+      console.log("Client not connected or chatRoomId not set");
     }
   };
 
@@ -271,7 +267,7 @@ const ChatRoom3 = () => {
                     {showProfileAndName && (
                       <View style={styles.friendMessageContainer}>
                         <Image
-                          source={require("../../assets/Avatar.png")}
+                          source={{ uri: message.profile }}
                           style={styles.avatar}
                         />
                         <Text style={styles.friendsName}>
