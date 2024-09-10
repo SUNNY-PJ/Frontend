4e72c81286bffa8273eea62dde1e98ff4239a000
yeoniii20 <commetoi_yeoni@naver.com>
2024-09-10 18:22:06 +0900

    refactor: 사용하지 않는 화면 경로 삭제

--- Navigate/ChatScreen.js
+++ Navigate/ChatScreen.js
@@ -2,7 +2,6 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import Chat from "../screen/Chat/chat";
-import Chat2 from "../screen/Chat/chat2";
 import Bottom from "../components/Bottom";
 import ChatRoom from "../screen/Chat/chatRoom";
 import ChatRoom3 from "../screen/Chat/chatRoom3";
@@ -17,7 +16,6 @@ function ChatScreen() {
       }}
     >
       {/* <Stack.Screen name="Chat" component={Chat} /> */}
-      <Stack.Screen name="Chat2" component={Chat2} />
       <Stack.Screen name="ChatRoom" component={ChatRoom} />
       <Stack.Screen name="ChatRoom3" component={ChatRoom3} />
     </Stack.Navigator>
