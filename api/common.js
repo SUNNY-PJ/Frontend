c8548c02c6017e518fe853d490355424302050a1
yeoniii20 <commetoi_yeoni@naver.com>
2024-09-16 23:39:23 +0900

    fix: server 주소 재변경

--- api/common.js
+++ api/common.js
@@ -2,5 +2,5 @@ export const REST_API_KEY = "7ff971db2010c97a3e191dd319ec45cd";
 export const REDIRECT_URI = "http://43.201.176.22:8080";
 export const INJECTED_JAVASCRIPT = `window.ReactNativeWebView.postMessage('message from webView')`;
 export const SOCKET_URI = "43.201.176.22:8080";
-// export const DEV_SOCKET_URI = "52.65.201.239:8080";
-export const DEV_SOCKET_URI = "3.35.101.15:8080";
+export const DEV_SOCKET_URI = "52.65.201.239:8080";
+// export const DEV_SOCKET_URI = "3.35.101.15:8080";
--- constant/common.js
+++ constant/common.js
@@ -1,7 +1,7 @@
 // export const proxyUrl = "http://43.201.176.22:8080";
-export const proxyUrl = "http://3.35.101.15:8080";
-// export const proxyUrl = "http://52.65.201.239:8080";
-export const dev_proxyUrl = "http://3.35.101.15:8080";
-// export const dev_proxyUrl = "http://52.65.201.239:8080";
+// export const proxyUrl = "http://3.35.101.15:8080";
+export const proxyUrl = "http://52.65.201.239:8080";
+// export const dev_proxyUrl = "http://3.35.101.15:8080";
+export const dev_proxyUrl = "http://52.65.201.239:8080";
 export const proxy_url = "43.201.176.22:8080";
 export const stompUrl = "http://localhost:8080/stomp";
